1d8fa897195bb8d01438e7ae64c2a8d546f30786
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-09-23 13:23:13 +1000

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,23 @@
-use std::fmt;
 use std::error::Error;
 use std::io;
 use std::path::PathBuf;
 use std::fs;
 use dirs::home_dir;
 
-enum HopError {
-    CouldNotReadFromHopDir,
-}
-
-impl fmt::Debug for HopError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-
-       let debug_info = match self {
-            HopError::CouldNotReadFromHopDir => "Hop Error D: Could not read from the .hop directory",
-        };
-
-        write!(f,"{}", debug_info)
-    }
-}
-
-impl fmt::Display for HopError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,"Hop Error2: {:?}",self)
-    }
-}
-
-impl Error for HopError {
-    fn description(&self) -> &str {
-        match self {
-            HopError::CouldNotReadFromHopDir => "Hop Error: Could not read from the .hop directory",
-        }
-    }
-}
 
 fn main() -> Result<(), Box<dyn Error>>{
     println!("~Hop~");
     let home = get_home()?;
     let home_entries = get_hop_home(home)?;
     for dir in home_entries {
-        let file_name = do_blah(dir).unwrap_or_else(|| "--unknown--".to_string());
+        let file_name = get_file_name(dir).unwrap_or_else(|| "--unknown--".to_string());
         println!("{}", file_name);
     }
 
     Ok(())
 }
 
-fn do_blah(dir: PathBuf) -> Option<String> {
+fn get_file_name(dir: PathBuf) -> Option<String> {
     Some(dir.file_name()?.to_str()?.to_string())
 }
 
@@ -67,6 +38,5 @@ fn get_hop_home(home: PathBuf) -> Result<Vec<PathBuf>, io::Error> {
     } else {
         Err(io::Error::new(io::ErrorKind::Other, "Could not get ~/.hop directory"))
     }
-
 
 }
