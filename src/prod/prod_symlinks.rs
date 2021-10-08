2800405eb031b50eef8d358a8d7596f2ecf37279
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-10-08 22:31:31 +1100

    Handle non-symlinks

--- src/prod/prod_symlinks.rs
+++ src/prod/prod_symlinks.rs
@@ -1,4 +1,5 @@
 use crate::models::{HopEffect, LinkPair, LinkTarget, Link};
+use crate::program::io_error;
 use super::prod_models::Prod;
 
 use crate::algebra::symlinks::{SymLinks, SymLink};
@@ -29,18 +30,34 @@ impl SymLinks for Prod {
     }
 }
 
+//TODO: Refactor this
 fn get_links(path: &PathBuf) -> HopEffect<Vec<LinkPair>> {
-    let x = fs::read_dir(path)?;
-    x.map(|res| res.and_then(|entry| create_link_pair(entry)))
-    .collect::<Result<Vec<_>, io::Error>>() //sequence
+    let dir_it = fs::read_dir(path)?;
+    let symlinks =
+        dir_it
+            .filter(|res| {
+                res
+                    .as_ref()
+                    .map_or_else(|_| false, |d| is_symlink(&d))
+            })
+            .map(|res| res.and_then(|entry| create_link_pair(&entry)))
+            .collect::<Result<Vec<_>, io::Error>>()?; //sequence
+
+    Ok(symlinks)
 }
 
-fn create_link_pair(dir_entry: DirEntry) -> HopEffect<LinkPair> {
-    let link_path = dir_entry.path();
-    //Choose to display a lossy string
-    let link = dir_entry.file_name().to_string_lossy().to_string();
-    //what if the file is not a link? We should filter those out
-    let target_res = fs::read_link(link_path);
+fn is_symlink(dir_entry: &DirEntry) -> bool {
+    dir_entry.path().symlink_metadata().map_or_else(|_| false, |meta| meta.file_type().is_symlink())
+}
 
-    target_res.map(|target| LinkPair{ link: Link(link), target: LinkTarget( target.to_string_lossy().to_string()) })
+fn create_link_pair(dir_entry: &DirEntry) -> HopEffect<LinkPair> {
+    let link_path = &dir_entry.path();
+    //Choose to display a lossy string
+    let link = &dir_entry.file_name().to_string_lossy().to_string();
+
+    let target_res = fs::read_link(link_path);
+    match target_res {
+        Ok(target) => Ok(LinkPair{ link: Link(link.to_string()), target: LinkTarget( target.to_string_lossy().to_string()) }),
+        Err(e)     => Err(io_error(&format!("Could not read link `{}` because: {}", link, e)))
+    }
 }
