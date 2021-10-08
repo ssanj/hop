cd22a368378dc9063fefb54fa596a8ceb2142929
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-10-08 11:46:10 +1100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use std::error::Error;
 use std::path::PathBuf;
-use std::fs::{self, DirEntry};
 use dirs::home_dir;
 use clap::{App, Arg};
 use models::{Link, LinkPair, LinkTarget};
-use std::path::Path;
-use std::os::unix::fs as nixfs;
 use std::io;
 use algebra::hop;
 use prod::prod_models::Prod;
@@ -16,7 +13,6 @@ mod models;
 mod algebra;
 mod prod;
 
-//todo: How do we test any?
 fn main() -> Result<(), Box<dyn Error>>{
     let app = App::new("Hop")
         .version("0.1.0")
@@ -56,8 +52,6 @@ fn main() -> Result<(), Box<dyn Error>>{
      let mut app2 = app.clone(); //we need this close to display usage on error
      let matches = app.get_matches();
 
-    let hop_home = get_home()?.join(".hop");
-
     let mut hop_program = hop::HopProgram { value: Prod, cfg_dir: ".hop".to_string() };
 
     let program =
@@ -82,8 +76,10 @@ fn main() -> Result<(), Box<dyn Error>>{
             }
 
         } else if let Some(d) = matches.value_of("delete") {
-            let _result = delete(&hop_home, Link(d.to_string()));
-            ()
+            match hop_program.delete_link(Link(d.to_string())) {
+                Ok(_) => (),
+                Err(e) => eprintln!("Could not delete link: {}", e)
+            }
         } else {
             let _result = app2.print_help();
             println!();
@@ -94,77 +90,4 @@ fn main() -> Result<(), Box<dyn Error>>{
 
 fn io_error(message: &str) -> io::Error {
     io::Error::new(io::ErrorKind::Other, message.clone())
-}
-
-fn prompt_user<Y, N, T>(message: &str, yes_action: Y, no_action: N) -> HopEffect<T> where
-    Y: FnOnce() -> HopEffect<T>,
-    N: FnOnce() -> HopEffect<T>
-{
-    println!("{}", message);
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    let response = buffer.lines().next().ok_or(io_error("Could not retrieve lines from stdio"))?;
-    match response {
-        "Y" | "y"  => yes_action(),
-        _ => no_action()
-    }
-}
-
-fn delete(hop_home: &PathBuf, link: Link) -> HopEffect<()> {
-
-    let result = match get_links(hop_home) {
-        Ok(link_pairs) => {
-           match link_pairs.iter().find(|lp| lp.link == link) {
-            Some(pair) => {
-                let prompt_message = format!("Are you sure you want to delete {} which links to {} ?", pair.link, pair.target);
-                let no_action = || Ok(println!("Aborting delete of {}", pair.link));
-                let yes_action = || {
-                    let file_path = (hop_home.clone()).join(&link);
-                    fs::remove_file(file_path)?;
-                    Ok(println!("Removed link {} which pointed to {}", &link, &pair.target))
-                };
-
-                prompt_user(&prompt_message, yes_action, no_action)?
-            },
-
-            None => eprintln!("Could not find link named:{} to delete", link)
-           }
-        },
-
-        Err(e) => println!("Could not retrieve links: {}", e)
-    };
-    Ok(result)
-}
-
-fn mark(hop_home: &PathBuf, pair: LinkPair) -> HopEffect<()> {
-    let symlink_path = (hop_home.clone()).join(&pair.link);
-
-    if symlink_path.exists() {
-        Ok(eprintln!("A link named `{}` already exists. Aborting mark creation.", &pair.link))
-    } else {
-        nixfs::symlink(&pair.target, symlink_path)
-            .map(|_| println!("Successfully created {} pointing to {}", &pair.link, pair.target))
-    }
-}
-
-
-fn get_home() -> Result<PathBuf, io::Error> {
-    home_dir().ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Could not get home directory"))
-}
-
-
-fn get_links(path: &PathBuf) -> HopEffect<Vec<LinkPair>> {
-    let x = fs::read_dir(path)?;
-    x.map(|res| res.and_then(|entry| create_link_pair(entry)))
-    .collect::<Result<Vec<_>, io::Error>>() //sequence
-}
-
-fn create_link_pair(dir_entry: DirEntry) -> HopEffect<LinkPair> {
-    let link_path = dir_entry.path();
-    //Choose to display a lossy string
-    let link = dir_entry.file_name().to_string_lossy().to_string();
-    //what if the file is not a link? We should filter those out
-    let target_res = fs::read_link(link_path);
-
-    target_res.map(|target| LinkPair{ link: Link(link), target: LinkTarget( target.to_string_lossy().to_string()) })
 }
