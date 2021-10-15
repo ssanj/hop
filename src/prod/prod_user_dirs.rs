a08c5132e7a856ee3af1398a6951bf1df3a21534
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-10-15 17:28:13 +1100

    Allow creation of hop home dir

--- src/prod/prod_user_dirs.rs
+++ src/prod/prod_user_dirs.rs
@@ -1,14 +1,36 @@
 use super::prod_models::Prod;
 use crate::algebra::user_dirs::UserDirs;
 use crate::models::HopEffect;
+use crate::program::{io_error, io_error_ex};
 use dirs::home_dir;
 
-use std::io;
+use std::{fs, io};
 use std::path::PathBuf;
 
 impl UserDirs for Prod {
     fn get_hop_home(&self, path: &str) -> HopEffect<PathBuf> {
-        Ok(get_home()?.join(path))
+        println!("===== getting home! ===");
+        let hop_home = get_home()?.join(path);
+
+         println!("===== checking meta! ===");
+
+
+        match fs::metadata(&hop_home) {
+            Ok(dir) =>
+                if dir.is_dir() {
+                    Ok(hop_home)
+                } else {
+                    Err(io_error(&format!("{} is not a directory", &hop_home.to_string_lossy())))
+                },
+            Err(e) => {
+                println!("===== creating dir! ===");
+                //~/.hop is not a directory, try and create it
+                match fs::create_dir(&hop_home) {
+                    Ok(_) => Ok(hop_home),
+                    Err(e) => Err(io_error_ex(&format!("Could not create dir: {}", &hop_home.to_string_lossy()), e)),
+                }
+            }
+        }
     }
 }
 
