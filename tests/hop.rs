320e0a8c7e80baa037f021eb921bb457b4eea85e
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-10-20 09:36:45 +1100

    Add test for link creation

--- tests/hop.rs
+++ tests/hop.rs
@@ -1,6 +1,7 @@
 use assert_cmd::Command; // Add methods on commands
 
 use std::fs;
+use std::io;
 use tempfile::{tempdir};
 
 #[test]
@@ -36,6 +37,37 @@ fn creates_hop_home() -> Result<(), Box<dyn std::error::Error>> {
     .success();
 
     fs::metadata(hop_home).expect(&format!("Could not find hop_home: {}", hop_home.to_string_lossy()));
+
+    Ok(())
+}
+
+ #[test]
+fn create_links() -> Result<(), Box<dyn std::error::Error>> {
+
+    let working_dir = tempdir()?;
+    let hop_home_temp = working_dir.path().join("mine").join("hophome");
+    let target_dir_temp = working_dir.path().join("somedir");
+    let hop_home = hop_home_temp.as_path();
+    let target_dir = target_dir_temp.as_path();
+
+    fs::create_dir_all(hop_home)?;
+    fs::create_dir_all(target_dir)?;
+    fs::metadata(hop_home).expect(&format!("Could not find hop_home: {}", hop_home.to_string_lossy()));
+
+    let mut cmd = Command::cargo_bin("hop")?;
+    cmd
+    .arg("-c")
+    .arg(hop_home.as_os_str())
+    .arg("-m")
+    .arg("blee")
+    .arg(target_dir.as_os_str())
+    .assert()
+    .success();
+
+    fs::metadata(target_dir).expect(&format!("Could not find target dir: {}", target_dir.to_string_lossy()));
+
+    let entries = fs::read_dir(hop_home)?.map(|res| res.map(|d| d.file_name())).collect::<Result<Vec<_>, io::Error>>()?;
+    assert_eq!(entries, vec!["blee"]);
 
     Ok(())
 }
