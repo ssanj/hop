1916e3b669176a7b358a8a16c4030b5936a6ff37
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-10-21 09:23:17 +1100

    Add test for jump

--- tests/hop.rs
+++ tests/hop.rs
@@ -4,7 +4,6 @@ use std::fs;
 use std::io;
 use tempfile::{tempdir};
 use std::os::unix::fs as nixfs;
-use predicates;
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -117,6 +116,54 @@ fn read_links() -> Result<(), Box<dyn std::error::Error>> {
     output_lines.sort();
 
     assert_eq!(&output_lines, &vec!["tag1", "tag2", "tag3"]);
+
+    working_dir.close()?;
+
+    Ok(())
+}
+
+#[test]
+fn retrieve_link() -> Result<(), Box<dyn std::error::Error>> {
+
+    let working_dir = tempdir()?;
+    let hop_home_temp = working_dir.path().join("mine").join("hophome");
+    let target_dir_temp1 = working_dir.path().join("somedir1");
+    let target_dir_temp2 = working_dir.path().join("somedir2");
+    let target_dir_temp3 = working_dir.path().join("somedir3");
+
+    let hop_home = hop_home_temp.as_path();
+    let target_dir1 = target_dir_temp1.as_path();
+    let target_dir2 = target_dir_temp2.as_path();
+    let target_dir3 = target_dir_temp3.as_path();
+
+    fs::create_dir_all(hop_home)?;
+    fs::create_dir_all(target_dir1)?;
+    fs::create_dir_all(target_dir2)?;
+    fs::create_dir_all(target_dir3)?;
+
+    fs::metadata(target_dir1).expect(&format!("Could not find target dir1: {}", target_dir1.to_string_lossy()));
+    fs::metadata(target_dir2).expect(&format!("Could not find target dir2: {}", target_dir2.to_string_lossy()));
+    fs::metadata(target_dir3).expect(&format!("Could not find target dir3: {}", target_dir3.to_string_lossy()));
+
+    nixfs::symlink(target_dir1, hop_home.join("tag1"))?;
+    nixfs::symlink(target_dir2, hop_home.join("tag2"))?;
+    nixfs::symlink(target_dir3, hop_home.join("tag3"))?;
+
+    let mut cmd = Command::cargo_bin("hop")?;
+
+    let output =
+        cmd
+        .arg("-c")
+        .arg(hop_home.as_os_str())
+        .arg("-j")
+        .arg("tag2")
+        .output()?;
+
+    let output_str = String::from_utf8(output.stdout)?;
+    let mut output_lines: Vec<&str> = output_str.lines().collect();
+    output_lines.sort();
+
+    assert_eq!(&output_lines, &vec![target_dir2.to_string_lossy()]);
 
     working_dir.close()?;
 
