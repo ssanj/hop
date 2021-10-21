b43e9f801275da2dd51d10ebd12cc5f083fd37d3
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-10-22 09:24:26 +1100

    Add test for deletes

--- tests/hop.rs
+++ tests/hop.rs
@@ -1,4 +1,4 @@
-use assert_cmd::Command; // Add methods on commands
+use assert_cmd::Command;
 
 use std::fs;
 use std::io;
@@ -164,6 +164,64 @@ fn retrieve_link() -> Result<(), Box<dyn std::error::Error>> {
     output_lines.sort();
 
     assert_eq!(&output_lines, &vec![target_dir2.to_string_lossy()]);
+
+    working_dir.close()?;
+
+    Ok(())
+}
+
+#[test]
+fn delete_link() -> Result<(), Box<dyn std::error::Error>> {
+
+    let working_dir = tempdir()?;
+    let hop_home_temp = working_dir.path().join("mine").join("hophome");
+    let target_dir_temp1 = working_dir.path().join("somedir1");
+    let target_dir_temp2 = working_dir.path().join("somedir2");
+    let target_dir_temp3 = working_dir.path().join("somedir3");
+
+    let hop_home = hop_home_temp.as_path();
+    let target_dir1 = target_dir_temp1.as_path();
+    let target_dir2 = target_dir_temp2.as_path();
+    let target_dir3 = target_dir_temp3.as_path();
+
+    fs::create_dir_all(hop_home)?;
+    fs::create_dir_all(target_dir1)?;
+    fs::create_dir_all(target_dir2)?;
+    fs::create_dir_all(target_dir3)?;
+
+    fs::metadata(target_dir1).expect(&format!("Could not find target dir1: {}", target_dir1.to_string_lossy()));
+    fs::metadata(target_dir2).expect(&format!("Could not find target dir2: {}", target_dir2.to_string_lossy()));
+    fs::metadata(target_dir3).expect(&format!("Could not find target dir3: {}", target_dir3.to_string_lossy()));
+
+    let tag1_temp = hop_home.join("tag1");
+    let tag2_temp = hop_home.join("tag2");
+    let tag3_temp = hop_home.join("tag3");
+
+    let tag1 = tag1_temp.as_path();
+    let tag2 = tag2_temp.as_path();
+    let tag3 = tag3_temp.as_path();
+
+    nixfs::symlink(target_dir1, tag1)?;
+    nixfs::symlink(target_dir2, tag2)?;
+    nixfs::symlink(target_dir3, tag3)?;
+
+    fs::metadata(tag1).expect(&format!("Could not find target tag1: {}", tag1.to_string_lossy()));
+    fs::metadata(tag2).expect(&format!("Could not find target tag2: {}", tag2.to_string_lossy()));
+    fs::metadata(tag3).expect(&format!("Could not find target tag3: {}", tag3.to_string_lossy()));
+
+    let mut command = Command::cargo_bin("hop")?;
+
+    command
+        .arg("-c")
+        .arg(hop_home.as_os_str())
+        .arg("-d")
+        .arg("tag2")
+        .write_stdin("Y".as_bytes())
+        .assert()
+        .success();
+
+
+    fs::metadata(tag2).expect_err("Find target tag2. Expected it to be deleted");
 
     working_dir.close()?;
 
